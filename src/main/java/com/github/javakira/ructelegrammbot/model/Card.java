1a3da6f6b8a392c612beb29a5b7811f212613af6
JavaKira <JavaKira@yandex.ru>
2023-04-10 23:22:43 +0300

    fix: rename Card employee -> employeeKey

--- src/main/java/com/github/javakira/ructelegrammbot/model/Card.java
+++ src/main/java/com/github/javakira/ructelegrammbot/model/Card.java
@@ -1,13 +1,8 @@
 package com.github.javakira.ructelegrammbot.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 import java.util.Date;
 import java.util.List;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
 public record Card(Date date, List<Pair> pairList) {
     public Card(Date date, List<Pair> pairList) {
         this.date = date;
