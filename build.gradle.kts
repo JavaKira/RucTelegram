bdd8ad1903cfaaae4309f84c6f7fc2fdbbfa33f6
JavaKira <JavaKira@yandex.ru>
2023-04-10 20:12:53 +0300

    add Spring Boot

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.github.javakira"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.telegram:telegrambots:6.5.0")
+    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.hibernate.orm:hibernate-core:6.2.0.Final")
+
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/com/github/javakira/ructelegrammbot/RucTelegrammBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javakira.ructelegrammbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RucTelegrammBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
